e3e93ce04e111d3cab52c411752ce5c7fd3a3bad
Oskar Røren <107056703+Te3Que@users.noreply.github.com>
2024-07-21 23:55:57 +0200

    endring i ordvalg
    fiksa litt på korleis det er skreve for og beire beskrive koden

--- main.py
+++ main.py
@@ -2,14 +2,8 @@ import math
 
 def finn_lengden(vektor):
     """
-    Denne funksjonen har i oppgave aa tar en vektor 
+    Denne funksjonen har i oppgave aa ta en vektor 
     som en liste og returnerer lengden på vektoren.
-
-    Args:
-        vektor: En liste som representerer en vektor med tall.
-
-    Returns:
-        Lengden på vektoren som et flyttall.
     """
 
     # Oppretter en variabel slik vi kan lagre kvadratsummen av elementene i vektoren
@@ -17,26 +11,26 @@ def finn_lengden(vektor):
 
     #  Her gaar vi over alle elementene i vektoren
     for tall in vektor:
-        # Kvadrer hvert element og legg til kvadratsummen
+        # Kvadrer hvert element og legger til kvadratsummen
         kvadratsum += tall ** 2
 
     # Beregn lengden ved å ta kvadratroten av kvadratsummen
     lengde = math.sqrt(kvadratsum)
 
-    # Returner den beregnede lengden
+    # Returner den beregna lengda
     return lengde
 
-# 2D vektorane vi skal rekne ut
+# 2D vektorane som er ment vi skal rekne ut
 a = [4, -3]
 b = [14, 5]
 c = [5, -3]
 
-# Finn lengden på vektorane a, b og c og lagre dem i variabler
-lengde_a = finn_lengden(a)
-lengde_b = finn_lengden(b)
-lengde_c = finn_lengden(c)
+# Finn lengda på vektorane a, b og c og lagre dem i variabler
+a_Lengden = finn_lengden(a)
+b_Lengden = finn_lengden(b)
+c_Lengden = finn_lengden(c)
 
 # Skriv ut produktet vi har reknet ut
-print(f"Lengden på vektor a er: {lengde_a}")
-print(f"Lengden på vektor b er: {lengde_b}")
-print(f"Lengden på vektor c er: {lengde_c}")
+print(f"Lengden på vektor a er: {a_Lengden}")
+print(f"Lengden på vektor b er: {b_Lengden}")
+print(f"Lengden på vektor c er: {c_Lengden}")
